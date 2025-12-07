4619ab668df8a463832178a13062c927d46c6b18
LENOVO <LENOVO@DESKTOP-ELQFVU0>
2025-12-07 10:19:39 +0800

    BarangControl

--- src/controller/BarangController.java
+++ src/controller/BarangController.java
@@ -3,7 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
-
+import connection.DBaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author LENOVO
